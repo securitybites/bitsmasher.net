8db947e05473fbb549e4da3052f1a4eebee42717
root <root@bitsmasher.net>
2017-01-24 18:25:30 +0000

    404 page

--- 404.php
+++ 404.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-body {
-    background-image:url("images/screen.jpg");
-    background-repeat:repeat;
-}
-</style>
 </head>
 <body>
+<center>
+<img src="images/confused.jpg">
+<br />
+<a href="https://www.bitsmasher.net/index.php"><h1>GO HOME</h1></a>
 
-
+</center>
 </body>
 </html>
