b4e4a0bb97e68e9bf973e7bdae1648b2481970f7
FrankTheTank <frank378@gmail.com>
2017-02-27 10:12:36 -0500

    Update nav_bar.php
    corrected URL for CTF site

--- bsidesindy/nav_bar.php
+++ bsidesindy/nav_bar.php
@@ -3,7 +3,7 @@
       <li><a href="index.php">Home</a></li>
 	   <li><a href="https://bsidesindy2017.busyconf.com/proposals/new" target="_blank">CFP</a></li>
 	   <li><a href="https://bsidesindy2017.busyconf.com/schedule" target="_blank">Schedule</a></li>
-	   <li><a class="active" href="ctf.wtf" target="_blank">CTF</a></li>
+	   <li><a class="active" href="https://ctf.wtf/" target="_blank">CTF</a></li>
       <li><a href="swag.php">SWAG</a></li>
 	  <li style="border-right: none;"><a href="sponsors.php">Sponsors</a></li>
 	</ul>
