19681d8328ccc46777901181f303f7b0b77ad429
TheDevilsVoice <thedevilsvoice@protonmail.ch>
2017-03-12 23:13:06 +0000

    remove hh

--- topnav.php
+++ topnav.php
@@ -5,9 +5,8 @@
 <ul>
   <li><a href="https://www.bitsmasher.net/index.php">Home</a></li>
   <li><a href="https://www.bitsmasher.net/bsidesindy/index.html">BsidesIndy</a></li>
-  <li><a href="https://www.bitsmasher.net/hackharder/">HackHarder</a></li>
   <li><a href="https://www.bitsmasher.net/sara2001conf/">Radio Astronomy</a></li>
-<li><a href="https://www.bitsmasher.net/projects/">Projects</a></li>
-<li><a href="https://www.bitsmasher.net/netlab/">Netlab</a></li>
+  <li><a href="https://www.bitsmasher.net/projects/">Projects</a></li>
+  <li><a href="https://www.bitsmasher.net/netlab/">Netlab</a></li>
   <li style="float:right"><a href="https://twitter.com/TheDevilsVoice">About</a></li>
 </ul>
