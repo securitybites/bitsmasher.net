3a9406d77273e0ecd03e14ebf2c4851bf9d4d7b0
thedevilsvoice <franklin@bitsmasher.net>
2017-10-22 12:44:43 -0400

    tweak sponsors page

--- bsidesindy/sponsors.php
+++ bsidesindy/sponsors.php
@@ -11,8 +11,6 @@
 
 <body>
 <div id="container">
-
-
   <div id="header">
     <div align="center"><img src="images/BSidesIndy_Skyline_lowres.png" alt="IndySkyline" width="510" height="215" border="0" /></div>
   </div>
@@ -23,60 +21,83 @@
   <div id="content">
     <div id="homeText">
           <p align="left">Thanks to RevICS Security</p><br /><br /><br /><br /><br /><br /><br /><br />
-	  <p align="left">Want to help make BSidesIndy possible?<br />
+          <p align="left">Want to help make BSidesIndy possible?<br />
         We'd love to hear from you!</p>
-      	<p>Download our <strong><a href="https://www.dropbox.com/s/9b9veak0dzunk3y/BSides_Indianapolis_Sponsorship_2017%20rev4.pdf?dl=0" target="blank">Sponsor Kit</a></strong><br />
-		and <a href="mailto:frank378@gmail.com"><strong>Email</strong></a> us. We're happy to<br />
-        answer your questions.<br /></p>
-	  </p>
+        <p><a href="mailto:frank378@gmail.com"><strong>Email</strong></a> us. We're happy to answer your questions.<br /></p>
+          </p>
     </div>
     <div id="contentPhoto">
       <div align="left"><br /><br /><br /><br />
          <a href="http://www.revics-security.com"><img src="images/revics-logo-cropped.png" alt="RevICS" width="275" /></a></div>
-      </div>
+    </div>
     
     <div id="contentPhoto">
       <div align="right"><br /><br /><br /><br />
          <img src="images/BSidesIndy_Logo_lowres.png" alt="flag" width="275" /></div>
-    </div>
-    </div>
-    
-    <div id="homeText">
-      <p align="left"><strong>What is BSidesIndy?</strong><br />
+    </div>  
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+
+
+    <div id="content">
+      <p ><strong>What is BSidesIndy?</strong><br />
         BSidesIndy applies the <a href="https://en.wikipedia.org/wiki/Security_BSides">Security BSides</a> model to a yearly conference in Indianapolis, IN. 
-		<p align="left"><strong>Why Sponsor BSidesIndy?</strong><br />
-		  We are a group of people who believe Information Security can be improved by strengthening the community. We do rely on the generosity of corporate
-		  sponsors, and at times wind up funding this event out of our own pockets. In return the community benefits in the form of increased interconnectedness, 
-		  bringing students, job seekers, employers, and seasoned industry pros together under one roof. 
-		</p>
-		<p>Since our first event on February 22, 2015, we've worked to add value to the industry and our community by providing free activities including 
-		speakers, resume workshops, IoT hacking village, lockpicking village, <a href="https://twitter.com/Hak4Kidz" target="_top">kid focused activities</a>, and 
-		more. In 2018 we hope to formalize the workshops we started last year into full trainings. The goal is to engage and educate present and future members of
-		the Information Security profession. We're noticing an uptick in attendees from local universities including Indiana University, Purdue, and IUPUI. This 
-		event drew about 200 local/regional participants last year, including staff and speakers from around the United States. 
-	   </p>
-	   <p>If you or your company are interested in helping us continue our mission, please consider contributing in a manner described below or reach out to us. 
-	   <a href="https://twitter.com/indybsides" target="_top">The quickest way to reach us is through our Twitter account</a>.
-	   </p>
-	  <p align="left"><strong><a href="https://imgur.com/a/w0UnE">Click here for pictures from our 2017 event.</a></strong></p>
+        <p align="left"><strong>Why Sponsor BSidesIndy?</strong><br />
+  We are a group of people who believe Information Security can be improved by strengthening the community. We do rely on the generosity of corporate
+  sponsors, and at times wind up funding this event out of our own pockets. In return the community benefits in the form of increased interconnectedness, 
+  bringing students, job seekers, employers, and seasoned industry pros together under one roof. 
+        </p>
+        <p>Since our first event on February 22, 2015, we've worked to add value to the industry and our community by providing free activities including 
+        speakers, resume workshops, IoT hacking village, lockpicking village, <a href="https://twitter.com/Hak4Kidz" target="_top">kid focused activities</a>, and 
+        more. In 2018 we hope to formalize the workshops we started last year into full trainings. The goal is to engage and educate present and future members of
+        the Information Security profession. We're noticing an uptick in attendees from local universities including Indiana University, Purdue, and IUPUI. This 
+        event drew about 200 local/regional participants last year, including staff and speakers from around the United States. 
+   </p>
+   <p>If you or your company are interested in helping us continue our mission, please consider contributing in a manner described below or reach out to us. 
+   <a href="https://twitter.com/indybsides" target="_top">The quickest way to reach us is through our Twitter account</a>.
+   </p>
+  <p align="left"><strong><a href="https://imgur.com/a/w0UnE">Click here for pictures from our 2017 event.</a></strong></p>
     </div>
-      <div id="content">
-          <p align="left">
-          <table align="center" border="1">
-          <tr><th>Cost</th><th>Level</th><th>Description</th></tr>
-          <tr><td>$1500</td><td>Badge Sponsor </td> <td>Sponsor name/logo silkscreened onto badge PCBs</td></tr>
-          <tr><td>$300</td><td>Lanyard Sponsor</td><td>Sponsor name/logo on lanyard</td></tr>
-          <tr><td>$500</td><td>Speaker/Staff Gift Sponsor</td><td>Black hoodies were pretty cool last year.</td></tr>
-          <tr><td>$1000</td><td>Breakfast Sponsor</td><td>Light breakfast for attendees.</td></tr>
-          <tr><td>$500</td><td>Challenge coin sponsor</td><td>100 coins, include sponsor logo.</td></tr>
-          <tr><td>$800</td><td>T-Shirt sponsor</td><td>This would get us about 150 shirts from Denver Screen Print.</td></tr>
-          <tr><td>$2000</td><td>staff/volunteer/keynote room sponsor</td> <td>Hotel rooms for people.</td></tr>
-          <tr><td>$3000</td> <td>venue sponsor</td> <td>Help cover the cost of the venue.</td></tr>
-          <tr><td>$1000</td> <td>workshop sponsor </td><td>Help cover travel/room cost for the three Friday workshop groups)</td></tr>
-          </table>
-           </p>
-       </div>
+    <div id="content">
+      <p align="left">
+      <table align="center" border="1">
+      <tr><th>Cost</th><th>Level</th><th>Description</th></tr>
+      <tr><td>$1500</td><td>Badge Sponsor </td> <td>Sponsor name/logo silkscreened onto badge PCBs</td></tr>
+      <tr><td>$300</td><td>Lanyard Sponsor</td><td>Sponsor name/logo on lanyard</td></tr>
+      <tr><td>$500</td><td>Speaker/Staff Gift Sponsor</td><td>Black hoodies were pretty cool last year.</td></tr>
+      <tr><td>$1000</td><td>Breakfast Sponsor</td><td>Light breakfast for attendees.</td></tr>
+      <tr><td>$500</td><td>Challenge coin sponsor</td><td>100 coins, include sponsor logo.</td></tr>
+      <tr><td>$800</td><td>T-Shirt sponsor</td><td>This would get us about 150 shirts from Denver Screen Print.</td></tr>
+      <tr><td>$2000</td><td>staff/volunteer/keynote room sponsor</td> <td>Hotel rooms for people.</td></tr>
+      <tr><td>$3000</td> <td>venue sponsor</td> <td>Help cover the cost of the venue.</td></tr>
+      <tr><td>$1000</td> <td>workshop sponsor </td><td>Help cover travel/room cost for the three Friday workshop groups)</td></tr>
+      </table>
+    </p>
     </div>
   <?php include "footer.php"; ?>
+</div>
 </body>
 </html>
